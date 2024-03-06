27ec0362f41ccd8bd413292c59b1a3e8e5fd386f
Max B <m.bergmann@students.htl-leonding.ac.at>
2024-03-06 15:26:20 +0100

    added server

---
+++ src/api/EasyProjectAPI/EasyProjectAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using EasyProjectAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add controllers to the service collection
+builder.Services.AddControllers();
+
+Setup.BuilderSetup(builder, "CorsPolicy");
+
+var app = builder.Build();
+
+Setup.SetUpEnvironment(app);
+
+app.MapControllers();
+
+app.Run();
---
+++ src/api/EasyProjectAPI/EasyProjectAPI/Setup.cs
@@ -0,0 +1,30 @@
+﻿namespace EasyProjectAPI;
+
+public class Setup
+{
+	public static void BuilderSetup(WebApplicationBuilder builder, string policy)
+	{
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+		builder.Services.AddSignalR();
+		builder.Services.AddCors(options =>
+		{
+			options.AddPolicy(policy, b =>
+			{
+				b.AllowAnyOrigin()
+					.AllowAnyMethod()
+					.AllowAnyHeader();
+			});
+		});
+	}
+
+	public static void SetUpEnvironment(WebApplication app)
+	{
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+	}
+}
+
